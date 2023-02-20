fcdeb7fd2788d254be79410d6e52cbae83bc8f2e
Jhana Barnes <jhanabarnes@gmail.com>
2023-02-20 01:18:52 -0500

    Added form and functionality

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import email
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -17,6 +21,18 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+  """Render the website's contact page."""
+  form = ContactForm()
+  if form.validate_on_submit():
+    msg = Message(request.form['subject'], sender=(request.form['name'], request.form['email']),recipients=['admin@example.com'])
+    msg.body = request.form['message']
+    mail.send(msg)
+    flash('Thank you for reaching out to us, your email was successfully sent.')
+    return redirect(url_for('home'))
+  return render_template('contact.html', form=form)
 
 ###
 # The functions below should be applicable to all Flask apps.
